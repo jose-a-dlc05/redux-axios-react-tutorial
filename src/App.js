108bace28a4c5aa640fd8d1386d91e314c16846e
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-10-10 19:31:24 -0400

    Added routing to app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
+import ProductDetails from './components/ProductDetails';
+import ProductListing from './components/ProductListing';
 
 function App() {
 	return (
 		<div>
-			<Header />
+			<Router>
+				<Header />
+				<Switch>
+					<Route path='/' exact component={ProductListing} />
+					<Route path='/product/:productId' component={ProductDetails} />
+					<Route>404 Not Found!</Route>
+				</Switch>
+			</Router>
 		</div>
 	);
 }
