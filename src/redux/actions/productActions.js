df5af0be58432d5b198b818e765b8fe398ed156f
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-10-10 17:58:00 -0400

    Added action creators to productActions.js file

---
+++ src/redux/actions/productActions.js
@@ -0,0 +1,18 @@
+// import Action Types from constants
+// create two action creators: SET_PRODUCTS & SELECTED_PRODUCT
+import { ActionTypes } from '../constants/action-types';
+const { SET_PRODUCTS, SELECTED_PRODUCT } = ActionTypes;
+
+export const setProducts = (products) => {
+	return {
+		type: SET_PRODUCTS,
+		payload: products,
+	};
+};
+
+export const selectedProduct = (product) => {
+	return {
+		type: SELECTED_PRODUCT,
+		payload: product,
+	};
+};
