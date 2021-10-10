cfea4bc990a40cd6ad8bb86fdd213202070ef461
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-10-10 18:08:04 -0400

    Added product reducer to productReducer.js file

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,21 @@
+import { ActionTypes } from '../constants/action-types';
+const { SET_PRODUCTS, SELECTED_PRODUCT } = ActionTypes;
+
+const initialState = {
+	products: [
+		{
+			id: 1,
+			title: 'Jose',
+			category: 'Developer',
+		},
+	],
+};
+
+const productReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case SET_PRODUCTS:
+			return state;
+		default:
+			return state;
+	}
+};
